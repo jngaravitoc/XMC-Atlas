f924e15de4c509b81c22b10a03704f1d54d969cd
jngaravitoc <jngaravitoc>
2025-12-01 17:00:01 -0500

    checking that the requested snapshots are in the folder

--- scripts/BFE_computation/ios_nbody_sims.py
+++ scripts/BFE_computation/ios_nbody_sims.py
@@ -1,6 +1,44 @@
 # TODO load bulge and disk particles
 import numpy as np
 import nba
+import os
+
+def check_files_in_folder(folder_path, expected_files):
+    """
+    Check that all expected files exist inside a folder.
+
+    Parameters
+    ----------
+    folder_path : str or Path
+        Path to the directory to check.
+    expected_files : list of str
+        Filenames expected to appear in the folder (exact matches).
+
+    Returns
+    -------
+    missing_files : list of str
+        Files that were expected but not found.
+
+    Raises
+    ------
+    FileNotFoundError
+        If any expected file is missing.
+    """
+    missing = []
+
+    for fname in expected_files:
+        full_path = os.path.join(folder_path, fname)
+        if not os.path.isfile(full_path):
+            missing.append(fname)
+
+    if missing:
+        raise FileNotFoundError(
+            f"The following files are missing in {folder_path}:\n{missing}"
+        )
+
+    return True
+
+
 
 class LoadSim:
     def __init__(self, snapshot_dir, snapname, origin_filename, suite):
@@ -14,6 +52,10 @@ class LoadSim:
         self.origin_filename = origin_filename
         self.suite = suite
 
+    def load_snap_time(self):
+        sim = nba.ios.ReadGadgetSim(self.snapshot_dir, self.snapname)
+        snap_time = sim.read_header()['Time']
+        return snap_time
 
     def load_mw_bulge(self, quantities=['pos', 'mass']):
         if self.suite == 'GC21':
@@ -32,6 +74,10 @@ class LoadSim:
         return disk_data
 
     def load_halo(self, halo,  quantities=['pos', 'mass'], npart=None):
+        """
+        halo : str
+            halo to which load the data
+        """
         if halo == 'MW' or'LMC':
             GC21_halo = nba.ios.ReadGC21(self.snapshot_dir, self.snapname)
             if npart:
@@ -55,3 +101,17 @@ class LoadSim:
             print("*Done loading {}".format(snapname))
 
         return halo_data
+
+
+
+def test_read_time():
+    SNAPSHOT_DIR = "/n/nyx3/garavito/XMC-Atlas-sims/GC21/MW/out/"
+    SNAP_NAME = "MW_100M_beta1_vir_OM3_G4_150.hdf5"
+    sim = LoadSim(SNAPSHOT_DIR, SNAP_NAME, "NONE", "NONE")
+    time = sim.load_snap_time()
+    assert time == 150*0.02
+
+if __name__ == "__main__":
+    test_read_time()
+
+
