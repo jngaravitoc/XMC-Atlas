ae8376fa6b31cb0b148b4c118922b12ba446e323
jngaravitoc <jngaravitoc@email.arizona.edu>
2026-03-17 23:34:36 -0400

    parallel job

--- scripts/exp_pipeline/compute_lmc_basis.sh
+++ scripts/exp_pipeline/compute_lmc_basis.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 set -e
+VALUES=(108)
 
-python build_spherical_basis.py 108 lmc --ncoefs=1 --fit-type=initial --output_dir=./
+export OMP_NUM_THREADS=1
+export MKL_NUM_THREADS=1
+export OPENBLAS_NUM_THREADS=1
+
+parallel -j 30 "python build_spherical_basis.py {} lmc --ncoefs=1 --fit-type=initial --output_dir ::: "${VALUES[@]}"
